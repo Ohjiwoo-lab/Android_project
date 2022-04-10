357834406a932431d972fbff5b21d25ab0e38a1c
OhJiwoo <74577768+Ohjiwoo-lab@users.noreply.github.com>
2022-04-10 15:33:25 +0900

    미션4 두 종류의 버튼 모양 만들기

---
+++ Mission5_4/app/src/main/java/com/cookandroid/mission5_4/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cookandroid.mission5_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("두 종류의 버튼 모양 만들기");
+    }
+}
