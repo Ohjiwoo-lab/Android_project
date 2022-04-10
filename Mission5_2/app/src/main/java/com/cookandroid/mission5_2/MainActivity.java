ccb7db76f9751798542676998eb3a7ae4de3f4c3
OhJiwoo <74577768+Ohjiwoo-lab@users.noreply.github.com>
2022-04-10 14:53:27 +0900

    미션2 위, 아래, 중앙의 공간을 차지하는 전형적인 화면 구성하기

---
+++ Mission5_2/app/src/main/java/com/cookandroid/mission5_2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cookandroid.mission5_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("2020039019 오지우");
+    }
+}
