26007b627cbe35230f860da360564849d28cdebf
OhJiwoo <74577768+Ohjiwoo-lab@users.noreply.github.com>
2022-04-24 17:35:56 +0900

    미션1 시크바와 프로그레스바 보여주기

---
+++ Mission6_1/app/src/main/java/com/cookandroid/mission6_1/MainActivity.java
@@ -0,0 +1,37 @@
+package com.cookandroid.mission6_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    public int number;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("시크바와 프로그레스바 보여주기");
+
+        final ProgressBar progressBar = findViewById(R.id.progressBar);
+        final EditText editText = findViewById(R.id.editText);
+        //SeekBar Listener 구현
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                progressBar.setProgress(progress);
+                editText.setText(new StringBuilder().append(progress));
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+    }
+}
